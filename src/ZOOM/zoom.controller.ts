3ee3761299cb406eae154afce34674d0460993df
gercel29121992 <gercelluciano@hotmail.com>
2024-05-18 20:34:44 -0400

    segunfpd

--- src/ZOOM/zoom.controller.ts
+++ src/ZOOM/zoom.controller.ts
@@ -9,6 +9,7 @@ import { JwtRole } from 'src/auth/jwt/jwt-Rol';
 import { JwtRolesGuard } from 'src/auth/jwt/jwt-roles.guard';
  
 import { ZoomService } from './zoom.service';
+import { updateZoomDto } from './dto/update-ZoomDto';
 
 @Controller('zoom')
 export class ZoomController {
@@ -22,6 +23,36 @@ constructor(private  zoomservices:ZoomService){}
      return this.zoomservices.findAll();
  
   }
+
+
+  @HasRoles(JwtRole.ADMIN,JwtRole.PROF)
+@UseGuards(JwtAuthGuard ,JwtRolesGuard)
+@Put(':id')
+update(@Param('id',ParseIntPipe) id:number,
+@Body() zoom:updateZoomDto){
+
+return this.zoomServices.update(id,zoom);
+}
+
+
+
+@HasRoles(JwtRole.ADMIN,JwtRole.PROF)
+@UseGuards(JwtAuthGuard ,JwtRolesGuard)
+@Put('upload/:id')
+@UseInterceptors(FileInterceptor('file'))
+updateWithImage(@UploadedFile( 
+  new ParseFilePipe({
+    validators: [
+      new MaxFileSizeValidator({ maxSize: 1024*1024*10 }),
+      new FileTypeValidator({ fileType: '.(png|jpeg|jpg)' }),
+    ],
+  }),
+) file: Express.Multer.File,
+@Param('id',ParseIntPipe) id:number,
+ @Body() zoom:updateZoomDto) {
+ 
+  return this.zoomServices.updateWithImage(file,id,zoom);
+}
  
 
 }
--- src/ZOOM/zoom.service.ts
+++ src/ZOOM/zoom.service.ts
@@ -4,6 +4,7 @@ import { Zoom } from './zoom.entity';
 import { Repository } from 'typeorm';
  
 import  storage = require( '../utils/cloud_storage');
+import { updateZoomDto } from './dto/update-ZoomDto';
 
 @Injectable()
 export class ZoomService {
@@ -18,5 +19,39 @@ export class ZoomService {
     }
  
 
+
+    
+    async update( id: number,zoom:updateZoomDto){
+        
+       
+        const zoomfound= await this.zoomRepository.findOneBy({id: id})
+        if(!zoomfound){
+         throw new HttpException('la categoria no se encuentra ',HttpStatus.NOT_FOUND);
+ 
+        }
+         const updatezoom = Object.assign(zoomfound,zoom);
+        return this.zoomRepository.save(updatezoom);
+      }
+
+      async updateWithImage(file: Express.Multer.File,id: number,zoom:updateZoomDto){
+        
+        const url = await  storage(file,file.originalname);
+        if(url ===undefined && url === null)
+        {
+          throw new HttpException('La imagen no se pudo guardar ',HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        const zoomfound= await this.zoomRepository.findOneBy({id: id})
+        if(!zoomfound){
+         throw new HttpException('la categoria no se encuentra ',HttpStatus.NOT_FOUND);
+ 
+        }
+        
+       
+       zoom.imagen=url;
+       const updatezoom = Object.assign(zoomfound,zoom);
+        return this.zoomRepository.save(updatezoom);
+      }
+
  
 }
+ 
