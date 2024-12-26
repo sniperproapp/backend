caafb63af5effcc42ece3afb9b39a8078033c827
sniperproapp <sniperproapp@gmail.com>
2024-12-26 09:57:44 -0400

    todo3

--- src/mails/mails.service.ts
+++ src/mails/mails.service.ts
@@ -13,12 +13,12 @@ private mailerservices: MailerService
 
   ){}
 
-  async senUserConfirmation( email:string){
+  async senUserConfirmation( email:string,token:string){
     await this.mailerservices.sendMail({
       to:email,
       subject:"Recuperar Password",
       template:'./recueperarpass',
-      context:{id:email}
+      context:{id:token}
       
     })
   }
