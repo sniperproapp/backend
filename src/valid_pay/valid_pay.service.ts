7a82092ca0775ce7808117dc285646cf61828884
sniperproapp <sniperproapp@gmail.com>
2025-09-15 14:51:09 -0400

    todo

--- src/valid_pay/valid_pay.service.ts
+++ src/valid_pay/valid_pay.service.ts
@@ -25,7 +25,14 @@ export class valid_payService {
 
 
      async update( data: any  ){
+        const fecha = new Date();
+        const mesActual = fecha.getMonth();
+        fecha.setMonth(mesActual + 1);
+        const anio = fecha.getFullYear();
+        const mes = (fecha.getMonth() + 1).toString().padStart(2, '0'); // +1 porque es base 0, y padStart para 2 dígitos
+        const dia = fecha.getDate().toString().padStart(2, '0');
 
+        const fechaFormateada = `${anio}-${mes}-10`;
 
      const saleinfo= await  this.saleRepository.findOne({where:{n_transaccion:data.order_id}})
      saleinfo.status=data.payment_status
@@ -37,18 +44,13 @@ export class valid_payService {
        const userinfo= await  this.usersRepository.findOne({where:{id:saleinfo.id_user }})
        userinfo.estado=1
        userinfo.estadoweb=1
+       userinfo.time_limit=new Date(fechaFormateada)
+       userinfo.time_limit_web=new Date(fechaFormateada)
        this.usersRepository.save(userinfo)
 
      }
      this.saleRepository.save(saleinfo)
-        const fecha = new Date();
-        const mesActual = fecha.getMonth();
-        fecha.setMonth(mesActual + 1);
-        const anio = fecha.getFullYear();
-        const mes = (fecha.getMonth() + 1).toString().padStart(2, '0'); // +1 porque es base 0, y padStart para 2 dígitos
-        const dia = fecha.getDate().toString().padStart(2, '0');
-
-        const fechaFormateada = `${anio}-${mes}-10`;
+      
         console.log(fechaFormateada)
      console.log("payment_id")    
      console.log(data.payment_id)  
