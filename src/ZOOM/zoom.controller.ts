ff79a092ca8b917df96f86b84e1362c69f2f8837
sniperproapp <sniperproapp@gmail.com>
2025-10-07 17:25:54 -0400

    todo

--- src/ZOOM/zoom.controller.ts
+++ src/ZOOM/zoom.controller.ts
@@ -11,10 +11,12 @@ import { JwtRolesGuard } from 'src/auth/jwt/jwt-roles.guard';
 import { ZoomService } from './zoom.service';
 import { updateZoomDto } from './dto/update-ZoomDto';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { EventsGateway } from 'src/comunication/events.gateway';
+import { LeyendaDto } from './dto/leyenda.dto';
 
 @Controller('zoom')
 export class ZoomController {
-constructor(private  zoomservices:ZoomService){}
+constructor(private  zoomservices:ZoomService,private readonly eventsGateway: EventsGateway){}
  
 @HasRoles(JwtRole.CLIENT,JwtRole.ADMIN,JwtRole.PROF
   )
@@ -55,7 +57,17 @@ updateWithImage(@UploadedFile(
   return this.zoomservices.updateWithImage(file,id,zoom);
 }
  
+ @Post('mostrar-leyenda')
+  mostrarLeyenda(@Body() leyendaDto: LeyendaDto) {
+    // 1. Recibimos la petición HTTP desde el panel de admin.
+    // 2. Llamamos al método público de nuestro Gateway.
+    this.eventsGateway.broadcastNuevaLeyenda("Operativa en Vivo");
+    
+    // 3. El Gateway se encarga de emitir el evento a todos los clientes.
+    return { success: true, message: 'Leyenda enviada a todos los usuarios.' };
+  }
 
+  
 }
 
  
--- src/ZOOM/zoom.module.ts
+++ src/ZOOM/zoom.module.ts
@@ -8,10 +8,11 @@ import { JwtStrategy } from 'src/auth/jwt/jwt.strategy';
 import { Zoom } from './zoom.entity';
 import { ZoomController } from './zoom.controller';
 import { ZoomService } from './zoom.service';
+import { EventsGateway } from 'src/comunication/events.gateway';
 
 @Module({
   imports:[TypeOrmModule.forFeature([Zoom ])],
-  providers: [ZoomService,JwtStrategy],
+  providers: [ZoomService,EventsGateway,JwtStrategy],
   controllers: [ZoomController]
 })
 export class ZoomModule {}
